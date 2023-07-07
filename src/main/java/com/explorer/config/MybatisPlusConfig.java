9c3195ed5916e493d64355a4465c8b74d44fdd7c
btwl <1253102853@qq.com>
2023-07-07 23:21:01 +0800

    just test

--- src/main/java/com/explorer/config/MybatisPlusConfig.java
+++ src/main/java/com/explorer/config/MybatisPlusConfig.java
@@ -5,14 +5,17 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MybatisPlusConfig {
 
   @Bean
+  @Profile("dev")
   public MybatisPlusInterceptor mybatisPlusInterceptor() {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
     interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
     return interceptor;
   }
+
 }
