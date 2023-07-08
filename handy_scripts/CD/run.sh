1df2eae85c708250cc15f1f609bae65cab28e60a
btwl <1253102853@qq.com>
2023-07-08 19:02:21 +0800

    testing scp rules

--- handy_scripts/CD/run.sh
+++ handy_scripts/CD/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 nohup java -jar explorer-test01.jar &
 sleep 1
-# shellcheck disable=SC2009
-if ps aux | grep -q "[e]xplorer-test01"; then
-  echo "explorer-test01 running success!"
-fi
+./ifRunning.sh
+
 
